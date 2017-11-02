374074dd60bff4b6986254f2a7b97dcb7906a308
Lauren Grillz <apps.grillz@gmail.com>
2017-11-01 21:21:49 -0500

    Add project files.

---
+++ TheWorld/Controllers/Api/TripsController.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheWorld.Models;
+using TheWorld.ViewModels;
+
+namespace TheWorld.Controllers.Api
+{
+    [Authorize]
+    [Route("api/trips")]
+    public class TripsController : Controller
+    {
+        private IWorldRepository _repository;
+        private ILogger<TripsController> _logger;
+
+        public TripsController(IWorldRepository repository, ILogger<TripsController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            try
+            {
+                var results = _repository.GetTripsByUsername(this.User.Identity.Name);
+
+                return Ok(Mapper.Map<IEnumerable<TripViewModel>>(results));
+            }
+            catch(Exception ex)
+            {
+                //TODO Logging
+                _logger.LogError($"Failed to get All trips: {ex}");
+
+                return BadRequest("Error occurred");
+            }
+        }
+
+        [HttpPost("")]
+        public async Task<IActionResult> Post([FromBody]TripViewModel theTrip)
+        {
+            if(ModelState.IsValid)
+            {
+                //Save to the Database
+                var newTrip = Mapper.Map<Trip>(theTrip);
+
+                newTrip.UserName = User.Identity.Name;
+
+                _repository.AddTrip(newTrip);
+
+                if(await _repository.SaveChangesAsync())
+                {
+                    return Created($"api/trips/{theTrip.Name}", Mapper.Map<TripViewModel>(newTrip));
+                }
+                else
+                {
+                    return BadRequest("Failed to save the trip");
+                }
+            }
+
+            return BadRequest(ModelState); //ModelState shouldn't be used in Production, probably
+ 
+        }
+    }
+}
---
+++ TheWorld/Startup.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Debug;
+using TheWorld.Services;
+using Microsoft.Extensions.Configuration;
+using TheWorld.Models;
+using Newtonsoft.Json.Serialization;
+using TheWorld.ViewModels;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Threading.Tasks;
+
+namespace TheWorld
+{
+    public class Startup
+    {
+        private IHostingEnvironment _env;
+        private IConfigurationRoot _config;
+
+        public Startup(IHostingEnvironment env)
+        {
+            _env = env;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(_env.ContentRootPath)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+
+            _config = builder.Build();
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(_config);
+
+            if (_env.IsEnvironment("Development"))
+            {
+                services.AddScoped<IMailService, DebugMailService>();
+            }
+            else
+            {             
+                //Implement a real Mail Service
+            };
+
+            //added to test env variables and minify/gulp
+            if (_env.IsEnvironment("Production"))
+            {
+                services.AddScoped<IMailService, DebugMailService>();
+            }
+            else if (_env.IsEnvironment("Staging"))
+            {
+                services.AddScoped<IMailService, DebugMailService>();
+            }
+            else
+            {
+                //Implement a real Mail Service
+            };
+
+            services.AddIdentity<WorldUser, IdentityRole>(config =>
+            {
+                config.User.RequireUniqueEmail = true;
+                config.Password.RequiredLength = 8;
+                config.Cookies.ApplicationCookie.LoginPath = "/Auth/Login";
+                config.Cookies.ApplicationCookie.Events = new CookieAuthenticationEvents()
+                {
+                    OnRedirectToLogin = async ctx =>
+                    {
+                        if(ctx.Request.Path.StartsWithSegments("/api") &&
+                         ctx.Response.StatusCode == 200)
+                        {
+                            ctx.Response.StatusCode = 401;
+                        }
+                        else
+                        {
+                            ctx.Response.Redirect(ctx.RedirectUri);
+                        }
+                        await Task.Yield();
+                    }
+                };
+            })
+            .AddEntityFrameworkStores<WorldContext>();
+
+            services.AddDbContext<WorldContext>();
+
+            services.AddScoped<IWorldRepository, WorldRepository>(); //Might have diff versions per scenario ie MockupRepo
+
+            services.AddTransient<GeoCoordsService>();
+
+            services.AddTransient<WorldContextSeedData>();
+
+            services.AddLogging();
+
+            services.AddMvc(config =>
+            {
+                if (_env.IsProduction())
+                {
+                    config.Filters.Add(new RequireHttpsAttribute()); //Adds filter to redirect you to https if you try http
+                }
+            })
+            .AddJsonOptions(config =>
+            {
+                config.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, 
+            IHostingEnvironment env,
+            WorldContextSeedData seeder,
+            ILoggerFactory factory)
+        {
+
+            if (env.IsEnvironment("Development"))
+            {
+                app.UseDeveloperExceptionPage();
+                factory.AddDebug(LogLevel.Information);
+            }
+            else
+            {
+                factory.AddDebug(LogLevel.Error);
+            }
+            app.UseStaticFiles();
+
+            //added for testing environmental variables and minify/gulp
+            if (env.IsEnvironment("Production"))
+            {
+                app.UseDeveloperExceptionPage();
+                factory.AddDebug(LogLevel.Information);
+            }
+            else if (env.IsEnvironment("Staging"))
+            {
+                app.UseDeveloperExceptionPage();
+                factory.AddDebug(LogLevel.Information);
+            }
+            else
+            {
+                factory.AddDebug(LogLevel.Error);
+            }
+            app.UseStaticFiles();
+            
+
+            app.UseIdentity();
+
+            Mapper.Initialize(config =>
+            {
+                config.CreateMap<TripViewModel, Trip>().ReverseMap();
+                config.CreateMap<StopViewModel, Stop>().ReverseMap();
+            });
+
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new
+                    {
+                        controller = "App",
+                        action = "Index"
+                    });
+            });
+
+            seeder.EnsureSeedData().Wait();
+        }
+    }
+}
